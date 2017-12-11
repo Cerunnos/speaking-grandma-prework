09b2cd4578d3b6ecb71e470df17b9d69f60f7ba3
declanm.mulligan@gmail.com Mulligan <declanm.mulligan@gmail.com>
2017-12-11 18:00:56 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -3,7 +3,8 @@ def speak_to_grandma(phrase)
     puts"I LOVE YOU TOO PUMPKIN"
   elsif phrase==phrase.uppercase
     puts"NO, NOT SINCE 1938"
-
+  else
+    puts "HUH?! SPEAK UP, SONNY!"
 end
 
 # Whatever you say to grandma, she should respond with
