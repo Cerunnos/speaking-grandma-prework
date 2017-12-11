b88d5cb0df88a9890b579f4f333f34d8a2974862
declanm.mulligan@gmail.com Mulligan <declanm.mulligan@gmail.com>
2017-12-11 17:59:52 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -1,7 +1,8 @@
 def speak_to_grandma(phrase)
   if phrase=="I LOVE YOU GRANDMA!"
+    puts"I LOVE YOU TOO PUMPKIN"
   elsif phrase==phrase.uppercase
-    puts "NO, NOT SINCE 1938"
+    puts"NO, NOT SINCE 1938"
 
 end
 
